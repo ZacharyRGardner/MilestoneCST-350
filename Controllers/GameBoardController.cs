ca498668f1c6dd3077d34fbdc63ff59b64213296
Kdkocian <krencent870@Gmail.com>
2022-07-02 23:11:24 -0500

    Added GameCell Model with MarkBombs, And Mark numbers

---
+++ Controllers/GameBoardController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MilestoneCST_350.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MilestoneCST_350.Controllers
+{
+    public class GameBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
